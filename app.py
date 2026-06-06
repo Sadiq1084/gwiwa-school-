53f4d77f383b4f852285b4827923ca45fe1281bc
Sadiq1084 <abubakarahussaini10@gmail.com>
2026-06-06 18:48:54 +0100

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ def home():
     news = [
         "Admission for 2026 is now open",
         "Mid-term exams start next Monday", 
-        "Parents meeting this Friday 2PM"
+        if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
     ]
     return render_template('index.html', school_name="Government junior Arabic secondary school Gwiwa ", news=news)
 
