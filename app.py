2f9904ced898a30276faa4b24663738bb667956e
Sadiq1084 <abubakarahussaini10@gmail.com>
2026-06-06 17:21:29 +0100

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello GWIWA School! Your app is live 🚀"
+    news = [
+        "Admission for 2026 is now open",
+        "Mid-term exams start next Monday", 
+        "Parents meeting this Friday 2PM"
+    ]
+    return render_template('index.html', school_name="GWIWA School", news=news)
 
 if __name__ == '__main__':
     app.run()
